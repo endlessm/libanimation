abc0141baceac23768c026d3f4356843d560441b
Sam Spilsbury <smspillaz@gmail.com>
2019-07-09 15:27:54 +0300

    animation-clutter: Add animation-clutter library and common helpers
    This library will be used to bind libanimation to Clutter, so that
    it can be used in GNOME-Shell. animation-clutter-common is a
    set of helper functions that are used to translate boxes into the
    correct space (eg, expanding paint boxes to cover the FBO size
    for a deform effect, determining the paint box extents of an
    actor, etc).

---
+++ animation-clutter/animation-clutter-common-private.h
@@ -0,0 +1,31 @@
+/*
+ * animation-clutter/animation-clutter-common-private.h
+ *
+ * Copyright © 2013-2016 Endless Mobile, Inc.
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * licence or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Sam Spilsbury <sam@endlessm.com>
+ */
+
+#pragma once
+
+#include <clutter/clutter.h>
+
+G_BEGIN_DECLS
+
+void animation_clutter_actor_box_enlarge_for_effects (ClutterActorBox *box);
+
+G_END_DECLS
+
